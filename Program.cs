0a496d65ce4c8eec2b5e5c2b8be6cd1b0ffe5a8e
Alok Sharma <56442060+alok744@users.noreply.github.com>
2024-05-28 12:52:34 +0530

    Sum_linked_list

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace two_sum
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            int[] num = { 3,3};
+            int num1;
+            for(int i=0; i<num.Length; i++)
+            {
+                for(int j=1;j<num.Length; j++)
+                {
+                    if (num[i] != num[j] || num[i] == num[j])
+                    {
+                        num1 = num[i] + num[j];
+                        Console.WriteLine(num1);
+                        
+                    }
+                    break;
+                }
+                break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
